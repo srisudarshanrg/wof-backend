a78dc8842e46645eb94e39086eccf8e8baf70c14
R Sri Sudarshan <srisudarshanrg@gmail.com>
2025-06-11 21:42:29 +0530

    changes

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	var app app.Application
 	app.DatabaseDSN = fmt.Sprintf("host=postgresql-raptor.alwaysdata.net dbname=raptor_wings_of_fire port=5432 user=raptor password=%s", dbPassword)
 	app.FrontendLink = "http://localhost:4000"
-	app.Port = fmt.Sprintf("localhost:%s", port)
+	app.Port = fmt.Sprintf("0.0.0.0:%s", port)
 
 	log.Println("Connecting to database")
 	db, err := app.ConnectDB()
