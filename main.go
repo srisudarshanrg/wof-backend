6ba7d8506625490528aa8970f343e0d174bbfdb8
R Sri Sudarshan <srisudarshanrg@gmail.com>
2025-06-11 21:44:52 +0530

    logging port

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 	port := os.Getenv("PORT")
 	dbPassword := os.Getenv("DATABASE_PASSWORD")
 
+	log.Println("Port:", port)
+
 	var app app.Application
 	app.DatabaseDSN = fmt.Sprintf("host=postgresql-raptor.alwaysdata.net dbname=raptor_wings_of_fire port=5432 user=raptor password=%s", dbPassword)
 	app.FrontendLink = "http://localhost:4000"
