3a98f857cf99832c79621b268640fcda61ece3fb
R Sri Sudarshan <srisudarshanrg@gmail.com>
2025-06-11 21:35:46 +0530

    change port

--- main.go
+++ main.go
@@ -12,12 +12,13 @@ import (
 
 func main() {
 	godotenv.Load(".env")
+	port := os.Getenv("PORT")
 	dbPassword := os.Getenv("DATABASE_PASSWORD")
 
 	var app app.Application
 	app.DatabaseDSN = fmt.Sprintf("host=postgresql-raptor.alwaysdata.net dbname=raptor_wings_of_fire port=5432 user=raptor password=%s", dbPassword)
 	app.FrontendLink = "http://localhost:4000"
-	app.Port = ":4444"
+	app.Port = fmt.Sprintf("localhost:%s", port)
 
 	log.Println("Connecting to database")
 	db, err := app.ConnectDB()
